3ca1e307af6ca6fe05e603f24219d94f5bd09189
PanyawatFaktim <s6704062612162@email.kmutnb.ac.th>
2025-12-16 13:53:30 +0700

    Bonus 2

--- lab3.c
+++ lab3.c
@@ -4,34 +4,44 @@
 int main() {
 	int a,b,c,d,i,x;
 	scanf("%d",&a);
-	if (a>=50) {
-		
-		if(a>=60){
-			
-			if(a>=70){
+	if(a>=0){
+		if(a<=100){
+			if (a>=50) {
 				
-				if(a>=80){
+				if(a>=60){
 					
-					printf("A\n");
+					if(a>=70){
+						
+						if(a>=80){
+							
+							printf("A\n");
+						}
+					else{
+						printf("B\n");
+						printf("%d more to get B",80-a);
+					}	
+					}
+				else{
+					printf("C\n");
+					printf("%d more to get B",70-a);
 				}
-			else{
-				printf("B\n");
-				printf("%d more to get B",80-a);
-			}	
+				}
+				else{
+					printf("D\n");
+					printf("%d more to get B",60-a);
+				} 
 			}
-		else{
-			printf("C\n");
-			printf("%d more to get B",70-a);
+			else{
+				printf("F\n");
+				printf("%d more to get D",50-a);
+			}
 		}
-		}
 		else{
-			printf("D\n");
-			printf("%d more to get B",60-a);
-		} 
+			printf("error score");
+		}
 	}
 	else{
-		printf("F\n");
-		printf("%d more to get D",50-a);
+		printf("error score");
 	}
 	return 0;
 }
